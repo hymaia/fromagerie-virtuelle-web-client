2378f861a950c07322e6536b604f2b2bb9dc0042
Simone Civetta <viteinfinite@gmail.com>
2023-01-01 20:50:50 +0100

    Add Tailwind

--- src/features/login/Login.tsx
+++ src/features/login/Login.tsx
@@ -1,43 +1,70 @@
-import { useContext, useState } from 'react';
-import { AccountContext } from '../../services/Account';
+import { useContext, useState } from "react";
+import { AccountContext } from "../../services/Account";
 
 function Login() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const { authenticate } = useContext(AccountContext);
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (authenticate == null) { return; }
+    if (authenticate == null) {
+      return;
+    }
     authenticate(username, password)
       .then((data: any) => {
-        alert('login success');
+        alert("login success");
         window.location.reload();
       })
       .catch((err: Error) => {
-        alert('login failure');
+        alert("login failure");
       });
   };
 
   return (
     <div>
       <form onSubmit={onSubmit}>
-        Username:
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <br />
-        Password:
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <br />
-        <button type="submit">Login</button>
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3">
+            <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+              Username:
+            </label>
+          </div>
+          <div className="md:w-2/3">
+            <input
+              type="text"
+              name="username"
+              value={username}
+              className="form-input bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3">
+            <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+              Password:
+            </label>
+          </div>
+          <div className="md:w-2/3">
+            <input
+              type="password"
+              name="password"
+              className="form-input bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Sign In
+          </button>
+        </div>
       </form>
     </div>
   );
--- src/features/register/Register.tsx
+++ src/features/register/Register.tsx
@@ -1,20 +1,20 @@
-import { CognitoUser, CognitoUserAttribute } from 'amazon-cognito-identity-js';
-import { useState } from 'react';
-import UserPool from '../../services/UserPool';
+import { CognitoUser, CognitoUserAttribute } from "amazon-cognito-identity-js";
+import { useState } from "react";
+import UserPool from "../../services/UserPool";
 
 function Register() {
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [verifyProcess, setVerifyProcess] = useState(false);
-  const [OTP, setOTP] = useState('');
+  const [OTP, setOTP] = useState("");
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const attributeList = [];
     attributeList.push(
       new CognitoUserAttribute({
-        Name: 'email',
+        Name: "email",
         Value: email,
       })
     );
@@ -23,7 +23,7 @@ function Register() {
         alert("Couldn't sign up");
       } else {
         setVerifyProcess(true);
-        alert('User Added Successfully');
+        alert("User Added Successfully");
       }
     });
   };
@@ -38,8 +38,8 @@ function Register() {
       if (err) {
         alert("Couldn't verify account");
       } else {
-        alert('Account verified successfully');
-        window.location.href = '/login';
+        alert("Account verified successfully");
+        window.location.href = "/login";
       }
     });
   };
@@ -48,28 +48,61 @@ function Register() {
     <div>
       {verifyProcess === false ? (
         <form onSubmit={onSubmit}>
-          UserName:
-          <input
-            type="text"
-            value={username.toLowerCase().trim()}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <br />
-          Email:
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <br />
-          Password:
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <br />
-          <button type="submit">Register</button>
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                Email:
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <input
+                type="email"
+                name="email"
+                className="form-input bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                value={username.toLowerCase().trim()}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                Email (Useless):
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <input
+                type="email"
+                className="form-input bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                Password:
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <input
+                type="password"
+                name="password"
+                className="form-input bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Sign up
+          </button>
         </form>
       ) : (
         <form onSubmit={verifyAccount}>
